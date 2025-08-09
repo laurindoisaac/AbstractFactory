ccd3e672a9d77db3600e2e20ff8c90932b0a269c
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 01:51:45 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/abstractfactory/main.go
@@ -0,0 +1,23 @@
+// cmd/abstractfactory/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "abstractfactory/internal/abstractfactory"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := abstractfactory.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
